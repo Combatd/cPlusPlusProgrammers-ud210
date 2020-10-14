473b6c25386671c3e73241b5a356a934d937818f
Combatd <combatdstar@gmail.com>
2020-10-14 02:52:38 -0700

    Use namespace from standard library

---
+++ basics/usingNamespace/usingNamespace/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  usingNamespace
+//
+//  Created by Mark Calvelo on 10/14/20.
+//
+
+/*Use the namespace keyword to simplify typing*/
+
+#include <iostream>
+using namespace std;
+
+/*
+int main() {
+    std::cout << "Hey, writing std:: is pain, ";
+    std::cout << "change the program so I don't have to write it.";
+    return 0;
+}
+*/
+
+int main() {
+    cout << "Hey, writing std:: is pain, ";
+    cout << "change the program so I don't have to write it.";
+}
