5215e88b62daf549dbee514b1410880f9ea03c9c
Combatd <combatdstar@gmail.com>
2020-10-14 02:43:38 -0700

    Print hello world

---
+++ basics/helloworld/helloworld/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  helloworld
+//
+//  Created by Mark Calvelo on 10/14/20.
+//
+
+/*write a C++ program that outputs the following statement:
+*** "Hello world, I am ready for C++"
+*/
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello world, I am ready for C++";
+    return 0;
+}
