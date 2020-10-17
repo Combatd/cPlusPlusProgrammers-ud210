0fda28ae9890b53f9e5345deefcfe072db0884b7
Combatd <combatdstar@gmail.com>
2020-10-17 16:40:19 -0700

    Include header file

---
+++ basics/HeaderFilesProgramming/HeaderFilesProgramming/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  HeaderFilesProgramming
+//
+//  Created by Mark Calvelo on 10/17/20.
+//
+
+#include "main.hpp"
+
+int main(int argc, const char * argv[]) {
+    cout<<"Hello, I use header files!";
+    return 0;
+}
