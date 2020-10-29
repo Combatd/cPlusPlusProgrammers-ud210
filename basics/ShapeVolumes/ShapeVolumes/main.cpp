521b3ee2919d7066c8fc419b88b49de156a28fd9
Combatd <combatdstar@gmail.com>
2020-10-29 11:40:36 -0700

    Import boilerplate for ShapeVolumes

---
+++ basics/ShapeVolumes/ShapeVolumes/main.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  ShapeVolumes
+//
+//  Created by Mark Calvelo on 10/29/20.
+//
+
+#include <iostream>
+#include <cmath>
+
+/*Goal: practice arithmetic operations in C++
+**Write a program that calculates the volumes of:
+**a cube, sphere, cone.
+**Cube Volume = side^3
+**Sphere Volume = (4/3) * pi * radius^3
+**Cone Volume = pi * radius^2 * (height/3)
+**Write the values to the console.
+*/
+
+int main() {
+    // Dimension of the cube
+    float cubeSide = 5.4;
+    // Dimension of sphere
+    float sphereRadius = 2.33;
+    // Dimensions of cone
+    float coneRadius = 7.65;
+    float coneHeight = 14;
+    
+    float volCube, volSphere, volCone = 0;
+    
+    return 0;
+}
