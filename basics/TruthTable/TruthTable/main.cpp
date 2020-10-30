19391db9497f9541e49582aa44e20ca293f4e862
Combatd <combatdstar@gmail.com>
2020-10-30 12:50:44 -0700

    Setup TruthTable project

---
+++ basics/TruthTable/TruthTable/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  TruthTable
+//
+//  Created by Mark Calvelo on 10/30/20.
+//
+
+/*Goal: use logical and relational operators in a C++ program.
+**Find all possible outputs for the given expression
+**Hint: there are three inputs, so there are 8 possible input combinations (2^3)
+**Which means there is an output for each combination of inputs.
+**The expression: (A and B and C) or (A and( (not B) or (not C))) = Q
+*/
+
+#include <iostream>
+
+
+int main()
+{
+    //(A and B and C) or (A and( (not B) or (not C))) = Q
+    
+    //Use this output format
+    // std::cout<<"A\tB\tC\t(A && B && C)\t\t(!B||!C)\t\t(A&&(!B||!C))\t\tQ\n";
+    return 0;
+}
