a4114f5b973f373cbc5a126e63979570bef22899
Combatd <combatdstar@gmail.com>
2020-10-31 00:00:12 -0700

    Define #PrintTruthTable

--- basics/TruthTable/TruthTable/main.cpp
+++ basics/TruthTable/TruthTable/main.cpp
@@ -14,12 +14,20 @@
 
 #include <iostream>
 
+void PrintTruthTable(int A, int B, int C);
 
-int main()
-{
+int main() {
     //(A and B and C) or (A and( (not B) or (not C))) = Q
+    int A, B, C = 0; // optionally chained variable declaration
+    
     
     //Use this output format
     // std::cout<<"A\tB\tC\t(A && B && C)\t\t(!B||!C)\t\t(A&&(!B||!C))\t\tQ\n";
+    std::cout<<"A\tB\tC\t(A && B && C)\t\t(!B||!C)\t(A&&(!B||!C))\tQ\n"; // let us print out the top of the table first
     return 0;
+}
+
+void PrintTruthTable(int A, int B, int C) {
+    int Q = (A && B && C) || (A && (!B || !C)); // the condition is an expression involving A B C to get a boolean
+    std::cout <<A << "\t" << B << "\t" << C << "\t\t" << (A && B && C) << "\t\t" << (!B||!C) << "\t\t" << (A&&(!B||!C)) << "\t\t" << Q << "\n";
 }
