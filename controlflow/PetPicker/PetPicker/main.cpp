1f9339e961e6eac79ff6f513172bf59eb612c535
Combatd <combatdstar@gmail.com>
2020-11-03 07:55:03 -0800

    Complete PetPicker Application

--- controlflow/PetPicker/PetPicker/main.cpp
+++ controlflow/PetPicker/PetPicker/main.cpp
@@ -16,20 +16,28 @@ int main(int argc, const char * argv[]) {
     std::cin>>skin;
     std::cout<<skin<<"\n";
     
-    //Use if-else statements to control program flow
-    
-    std::cout<<"Get a dog"<<"\n";
-    std::cout<<"Get a bird"<<"\n";
-    
-    std::cout<<"Would you like an animal that lives in water(w), land(l), or both(b)?";
-    std::cin>>location;
-    std::cout<<location<<"\n";
-    
-    //Use if-else statements to control program flow
-    
-    std::cout<<"Get a fish"<<"\n";
-    std::cout<<"Get a gecko"<<"\n";
-    std::cout<<"Get a frog"<<"\n";
-    
+    // Use if-else statements to control program flow
+    // cin by default is C String format!
+    if (skin == 'f') {
+        std::cout<<"Get a dog"<<"\n";
+    } else if (skin == 't') {
+        std::cout<<"Get a bird"<<"\n";
+    } else if (skin == 's') {
+        std::cout<<"Would you like an animal that lives in water(w), land(l), or both(b)?";
+        std::cin>>location;
+        std::cout<<location<<"\n";
+        // Use if-else statements to control program flow
+        if (location == 'w') {
+            std::cout << "Get a fish"<<"\n";
+        } else if (location == 'l') {
+            std::cout << "Get a gecko" <<"\n";
+        } else if (location == 'b') {
+            std::cout<< "Get a frog" <<"\n";
+        } else {
+            std::cout << "Enter water(w), land(l), or both (b)\n";
+        }
+    } else {
+        std::cout << "Enter fur(f), feathers(t), scales(s)" << "\n";
+    }
     return 0;
 }
