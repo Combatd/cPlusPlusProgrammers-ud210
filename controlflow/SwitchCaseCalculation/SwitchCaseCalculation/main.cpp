612de16671f3c7dd37fb2f9cab73128cb7988e40
Combatd <combatdstar@gmail.com>
2020-11-04 11:22:30 -0800

    Create SwitchCaseCalculation Project

---
+++ controlflow/SwitchCaseCalculation/SwitchCaseCalculation/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  SwitchCaseCalculation
+//
+//  Created by Mark Calvelo on 11/4/20.
+//
+
+/*Create a program that asks the user for
+**two float numbers. Then asks the user if they would like to:
+**add the numbers, subtract the numbers, multiply the numbers,
+**divide the numbers.
+**The program should then print the numbers with the chosen
+**operation and the solution.
+*/
+
+#include <iostream>
+
+int main() {
+    float in1, in2;
+    std::cout<<"Enter two numbers:\n";
+    std::cin>>in1;
+    std::cin>>in2;
+    std::cout<<"Enter the operation '+','-','*','/':\n";
+    return 0;
+}
