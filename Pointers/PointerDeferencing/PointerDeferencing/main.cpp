5ef408b9ab5e1b3bbcf06e13682888c0777b4836
Combatd <combatdstar@gmail.com>
2020-11-05 14:12:47 -0800

    Create PointerDereferencing project

---
+++ Pointers/PointerDeferencing/PointerDeferencing/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  PointerDeferencing
+//
+//  Created by Mark Calvelo on 11/5/20.
+//
+/*For this program print for each variable
+**print the value of the variable,
+**then print the address where it is stored.
+*/
+
+#include <iostream>
+#include <string>
+
+int main() {
+    int givenInt;
+    float givenFloat;
+    double givenDouble ;
+    std::string givenString;
+    char givenChar;
+    
+    return 0;
+}
