7b4cfeffd1c0e9bcdd48c114684d338e6521dc67
Combatd <combatdstar@gmail.com>
2020-11-06 07:52:39 -0800

    Setup minmaxpractice

---
+++ Arrays/MinMaxPractice/MinMaxPractice/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  MinMaxPractice
+//
+//  Created by Mark Calvelo on 11/6/20.
+//
+
+/*Find the min and max and average of 15 numbers that a user will input.
+**The numbers range from 0 to 100.
+**We will do it now for practice and again later when we learn arrays and
+**functions. So you do not have to keep all fifteen numbers stored in memory.
+**HINT: you will need to use scanf for input text.
+*/
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
